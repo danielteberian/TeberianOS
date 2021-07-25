ec63a1d4eb17c4b38ef94695366e9397d4d04bd5
danielteberian <danielteberian@github.com>
2021-07-25 02:20:02 -0500

    I added a bit of code to the src/vga_buffer.rs file, and I have seemingly done something, as I have added a duplicate line, right under the original line/message. The only difference is that the first word of the second line is garbled, or (perhaps) an error code or something. It is late, and I have been working on this all day.

--- teberianos/src/vga_buffer.rs
+++ teberianos/src/vga_buffer.rs
@@ -76,7 +76,18 @@ impl Writer {
 		}
 	}
 
-	fn new_line(&mut self) {/* no idea */}
+	fn new_line(&mut self) {
+		for row in 1..BUFFER_HEIGHT {
+			for col in 0..BUFFER_WIDTH {
+				let character = self.buffer.chars[row][col].read();
+				self.buffer.chars[row - 1][col].write(character);
+				}
+			}
+			self.clear_row(BUFFER_HEIGHT - 1);
+			self.column_position = 0;
+		}
+
+		fn clear_row(&mut self, row: usize) { /* idk */ }
 }
 
 
@@ -96,7 +107,7 @@ impl Writer {
 
 pub fn all_good() {
 	use core::fmt::Write;
-	let mut writer = Writer {
+let mut writer = Writer {
 		column_position: 0,
 		color_code: ColorCode::new(Color::Yellow, Color::Black),
 		buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
