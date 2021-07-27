1c438a3fde0f19b5f7743617f10b61f04abb44dc
danielteberian <danielteberian@github.com>
2021-07-26 21:01:39 -0500

    I fixed an issue, with the syntax, that was preventing the kernel from being compiled.

--- teberianos/src/main.rs
+++ teberianos/src/main.rs
@@ -2,19 +2,35 @@
 #![no_std]
 //Disables the use of the normal entry point chain.
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(teberian_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
 
 //Instructs the kernel to use the included panic handler.
 use core::panic::PanicInfo;
-
 mod vga_buffer;
 
 //The first thing that my operating system has ever done.
 static ALIVE: &[u8] = b"I am alive!";
 
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+	println!("RUNNING {} TESTS", tests.len());
+	for test in tests {
+		test();
+	}
+}
+
+
 //This is a replacement for the main function.
 #[no_mangle] //Leave the name of this function alone! >:(
 pub extern "C" fn _start() -> ! {
 	println!("Still going, but with better code{}", "!");
+
+	#[cfg(test)]
+	test_main();
 
 	loop {}
 }
